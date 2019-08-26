0363aabb070209ba158db9f6b254de3dbe1d960b
Santana Jolly <santanaharris@gmail.com>
2019-08-26 10:41:35 -0400

    Add project files.

---
+++ Assignment.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Manipulating_Arrays
+{
+    class Assignment
+    {
+        public void Display(int[] array)
+        {
+            Console.Write(string.Join(", ", array));
+        }
+        public void Count(int[] array)
+        {
+            Console.WriteLine($"\nThere are {array.Length} values in this array");
+        }
+        public int Sum(int[] array)
+        {
+            int sum = 0;
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+        public void Mean(int[] array, int sum)
+        {
+            double mean = ((double)sum / array.Length);
+            Console.WriteLine($"The sum is {sum}");
+            Console.WriteLine($"The mean is {mean}");
+        }
+        public void Reverse(int[] array)
+        {
+            Console.Write("Reverse: ");
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.Write(array[i]);
+            }
+        }
+        public int[] Sort(int[] array)
+        {
+            int temp = 0;
+            Console.Write("\nSort: ");
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+        //create temp array
+        //if statement for string left or right
+        public int[] Rotate(int[] array, int shift, string direction)
+        {
+            Console.Write($"\nRotated {direction} {shift} places: ");
+            int[] temp = new int[array.Length];
+            if (direction == "right" || direction == "RIGHT")
+            {
+                for (int i = 0, j = shift; i < array.Length; i++, j++)
+                {
+                    if (j > array.Length - 1)
+                    {
+                        j = array.Length - j;
+                        temp[j] = array[i];
+                    }
+                    else
+                    {
+                        temp[j] = array[i];
+                    }
+                }
+                return array;
+            }
+            else
+            {
+                for (int j = 0, i = shift; j < array.Length; i++, j++)
+                {
+                    if (i > array.Length - 1)
+                    {
+                        i = array.Length - i;
+                        temp[i] = array[j];
+                    }
+                    else
+                    {
+                        temp[i] = array[j];
+                    }
+
+                }
+                
+                return array;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_6A___Manipulating_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayA = new int[6] { 0, 2, 4, 6, 8, 10 };
+            int[] arrayB = new int[5] { 1, 3, 5, 7, 9 };
+            int[] arrayC = new int[12] { 3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 9 };
+            var assignment = new Assignment();
+            int sumA = assignment.Sum(arrayA);
+            int sumB = assignment.Sum(arrayB);
+            int sumC = assignment.Sum(arrayC);
+
+
+            Console.WriteLine("Array A: ");
+            assignment.Display(arrayA);
+            assignment.Count(arrayA);
+            assignment.Sum(arrayA);
+            assignment.Mean(arrayA, sumA);
+            assignment.Reverse(arrayA);
+            int[] arraySA = assignment.Sort(arrayA);
+            assignment.Display(arraySA);
+            int shift = 2;
+            string direction = "left";
+            int[] arrayRA = assignment.Rotate(arrayA, shift, direction);
+            assignment.Display(arrayRA);
+            
+
+            Console.WriteLine("\n\nArray B: ");
+            assignment.Display(arrayB);
+            assignment.Count(arrayB);
+            assignment.Sum(arrayB);
+            assignment.Mean(arrayB, sumB);
+            assignment.Reverse(arrayB);
+            int[] arraySB = assignment.Sort(arrayB);
+            assignment.Display(arraySB);
+            shift = 2;
+            direction = "right";
+            int[] arrayRB = assignment.Rotate(arrayB, shift, direction);
+            assignment.Display(arrayRB);
+
+            Console.WriteLine("\n\nArray C: ");
+            assignment.Display(arrayC);
+            assignment.Count(arrayC);
+            assignment.Sum(arrayC);
+            assignment.Mean(arrayC, sumC);
+            assignment.Reverse(arrayC);
+            //int[] arraySC = assignment.Sort(arrayC);
+            //assignment.Display(arraySC);
+            shift = 4;
+            direction = "left";
+            int[] arrayRC = assignment.Rotate(arrayC, shift, direction);
+            assignment.Display(arrayRC);
+
+        }
+
+    }
+}
